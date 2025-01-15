cea4462a84a91d5107e384a168bb9b7955b0c606
Martin Lind <marlin00.Work@gmail.com>
2025-01-15 14:10:32 +0100

    Done with Exercise 1, refactored the classes and made my own tests

--- tdd-oop-inheritance.CSharp.Main/Library.cs
+++ tdd-oop-inheritance.CSharp.Main/Library.cs
@@ -7,27 +7,26 @@ using System.Threading.Tasks;
 namespace tdd_oop_inheritance.CSharp.Main
 {
     public class Library {
+        /*
         List<Article> articles = new List<Article>();
         List<Book> books = new List<Book>();
         List<Newspaper> newspapers = new List<Newspaper>();
+        */
 
-        public void addToStock(Article item) {
-            this.articles.Add(item);
+        private List<Literature> literatures;
+
+        public Library() 
+        { 
+            literatures = new List<Literature>();
+        }
+        public void addToStock(Literature item) 
+        {
+            this.literatures.Add(item);
         }
 
-        public void addToStock(Book item) {
-            this.books.Add(item);
-        }
+        public string checkInLiterature(string title) {
 
-        public void addToStock(Newspaper item) {
-            this.newspapers.Add(item);
-        }
-
-        // The following methods may contain code that you are unfamiliar with. The strange syntax of article -> something
-        // is called a lambda expression (https://www.w3schools.com/java/java_lambda.asp)
-        public string checkInArticle(string title) {
-
-            List<Article> filtered = (List<Article>)this.articles.Where(article => article.title.Equals(title));
+            List<Literature> filtered = literatures.Where(literature => literature.Title.Equals(title)).ToList();
 
             if (filtered.Count() < 1) {
                 return "item is not part of the library's collection";
@@ -36,48 +35,8 @@ namespace tdd_oop_inheritance.CSharp.Main
             return filtered[0].checkIn();
         }
 
-        public string checkOutArticle(string title) {
-            List<Article> filtered = (List<Article>)this.articles.Where(article => article.title.Equals(title));
-
-            if (filtered.Count() < 1) {
-                return "item is not part of the library's collection";
-            }
-
-            return filtered[0].checkOut();
-        }
-
-        public string checkInBook(string title) {
-            List<Book> filtered = (List<Book>)this.books.Where(book => book.Equals(title));
-
-            if (filtered.Count() < 1) {
-                return "item is not part of the library's collection";
-            }
-
-            return filtered[0].checkIn();
-        }
-
-        public string checkOutBook(string title) {
-            List<Book> filtered = (List<Book>)this.books.Where(book => book.Equals(title));
-
-            if (filtered.Count() < 1) {
-                return "item is not part of the library's collection";
-            }
-
-            return filtered[0].checkOut();
-        }
-
-        public string checkInNewspaper(string title) {
-            List<Newspaper> filtered = (List<Newspaper>)this.newspapers.Where(newspaper => newspaper.title.Equals(title));
-
-            if (filtered.Count() < 1) {
-                return "item is not part of the library's collection";
-            }
-
-            return filtered[0].checkIn();
-        }
-
-        public string checkOutNewspaper(string title) {
-            List<Newspaper> filtered = (List<Newspaper>)this.newspapers.Where(newspaper => newspaper.title.Equals(title));
+        public string checkOutLiterature(string title) {
+            List<Literature> filtered = literatures.Where(literature => literature.Title.Equals(title)).ToList();
 
             if (filtered.Count() < 1) {
                 return "item is not part of the library's collection";
---
+++ tdd-oop-inheritance.CSharp.Test/MyTests.cs
@@ -0,0 +1,111 @@
+using tdd_oop_inheritance.CSharp.Main;
+using NUnit.Framework;
+
+namespace tdd_oop_inheritance.CSharp.Test
+{
+    public class MyTests
+    {
+        [Test]
+        public void TestCheckInNotLoanedBook()
+        {
+            Library lib = new Library();
+
+            lib.addToStock(new Book("Harry Potter"));
+            lib.addToStock(new Book("Lord of the rings"));
+            lib.addToStock(new Article("Science"));
+            lib.addToStock(new Newspaper("Robbery somewhere"));
+            lib.addToStock(new Newspaper("Its gonna rain, like.. alot..."));
+
+            string search = "Science";
+
+
+            Assert.That(lib.checkInLiterature(search), Is.EqualTo("item is not currently on loan"));
+        }
+
+        [Test]
+        public void TestCheckIn()
+        {
+            Library lib = new Library();
+
+            lib.addToStock(new Book("Harry Potter"));
+            lib.addToStock(new Book("Lord of the rings"));
+            lib.addToStock(new Article("Science"));
+            lib.addToStock(new Newspaper("Robbery somewhere"));
+            lib.addToStock(new Newspaper("Its gonna rain, like.. alot..."));
+
+            string search = "Science";
+            lib.checkOutLiterature(search);
+
+
+            Assert.That("item has been checked in", Is.EqualTo(lib.checkInLiterature(search)));
+        }
+
+        [Test]
+        public void TestCheckInBookNotExisting()
+        {
+            Library lib = new Library();
+
+            lib.addToStock(new Book("Harry Potter"));
+            lib.addToStock(new Book("Lord of the rings"));
+            lib.addToStock(new Article("Science"));
+            lib.addToStock(new Newspaper("Robbery somewhere"));
+            lib.addToStock(new Newspaper("Its gonna rain, like.. alot..."));
+
+            string search = "Starwars";
+
+
+            Assert.That("item is not part of the library's collection", Is.EqualTo(lib.checkInLiterature(search)));
+        }
+
+        [Test]
+        public void TestCheckOutBookIn()
+        {
+            Library lib = new Library();
+
+            lib.addToStock(new Book("Harry Potter"));
+            lib.addToStock(new Book("Lord of the rings"));
+            lib.addToStock(new Article("Science"));
+            lib.addToStock(new Newspaper("Robbery somewhere"));
+            lib.addToStock(new Newspaper("Its gonna rain, like.. alot..."));
+
+            string search = "Science";
+
+            Assert.That(lib.checkOutLiterature(search), Is.EqualTo("item has been checked out"));
+        }
+
+        [Test]
+        public void TestCheckOutBookLoaned()
+        {
+            Library lib = new Library();
+
+            lib.addToStock(new Book("Harry Potter"));
+            lib.addToStock(new Book("Lord of the rings"));
+            lib.addToStock(new Article("Science"));
+            lib.addToStock(new Newspaper("Robbery somewhere"));
+            lib.addToStock(new Newspaper("Its gonna rain, like.. alot..."));
+
+            string search = "Science";
+
+            lib.checkOutLiterature(search);
+
+            Assert.That(lib.checkOutLiterature(search), Is.EqualTo("item is currently on loan"));
+        }
+
+        [Test]
+        public void TestCheckOutBookNotExisting()
+        {
+            Library lib = new Library();
+
+            lib.addToStock(new Book("Harry Potter"));
+            lib.addToStock(new Book("Lord of the rings"));
+            lib.addToStock(new Article("Science"));
+            lib.addToStock(new Newspaper("Robbery somewhere"));
+            lib.addToStock(new Newspaper("Its gonna rain, like.. alot..."));
+
+            string search = "Starwars";
+
+
+            Assert.That(lib.checkOutLiterature(search), Is.EqualTo("item is not part of the library's collection"));
+        }
+    }
+}
